d8f1ca19014918ae9cd09e2132faf4c69b556ccf
Degmin49 <degmin49@gmail.com>
2022-05-06 15:57:40 +0200

    Test2

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-          Daniel W
+          TEST
         </a>
       </header>
     </div>
